55d0dd2483366050b58cf2b256b3dc9c74703b65
Manan Patel <mananpatel.711@gmail.com>
2024-04-05 08:40:50 -0400

    Shopping Cart complete

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { useCart } from '@/providers/CartProvider';
 import CartListItem from '@/components/CartListItem';
+import Button from '@/components/Button';
 
 
 const CartScreen = () => {
-    const { items } = useCart()
+    const { items, total } = useCart()
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={items}
         renderItem={({item}) => <CartListItem cartItem={item} />}
-        contentContainerStyle={{padding: 10, gap: 10}}
+        contentContainerStyle={{ gap: 10}}
         />
+        <Text style={styles.total}>Total: ${total.toFixed(2)}</Text>
+        <Button text='Checkout' onPress={()=> console.warn('Checkout')} />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   )
@@ -21,4 +24,14 @@ const CartScreen = () => {
 
 export default CartScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        padding: 10
+    },
+    total: {
+        textAlign: 'right',
+        fontWeight: '500',
+        fontSize: 20,
+        marginTop: 10
+    }
+})
