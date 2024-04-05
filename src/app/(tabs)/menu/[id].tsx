b4e6b6c8d00ca464d91888da42b9f1c727eecb03
Manan Patel <mananpatel.711@gmail.com>
2024-04-05 08:18:23 -0400

    Shopping Cart

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,19 +1,25 @@
 import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
 import React, { useState } from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router'
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router'
 import products from '@assets/products'
 import { defaultPizzaImage } from '@/components/ProductListItem'
 import Button from '@/components/Button'
+import { useCart } from '@/providers/CartProvider'
+import { PizzaSize } from '@/types'
 
-const sizes = ['S', 'M', 'L', 'XL']
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL']
 
 const ProductDetailsScreen = () => {
+  const router = useRouter()
   const { id } = useLocalSearchParams()
-  const [selectedSize, setSelectedSize] = useState('M')
+  const {addItem} = useCart()
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>('M')
   const product = products.find(p => p.id.toString() === id)
 
   const addToCart = () => {
-    console.warn('Adding to cart, size: ', selectedSize)
+    if(!product) return
+    addItem(product, selectedSize)
+    router.push('/cart')
   }
 
   if(!product) return <Text>Product not found</Text>
