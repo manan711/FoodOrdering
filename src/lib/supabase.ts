c160c2fd3b62066631a2917df3d1d331f66c29ae
Manan Patel <mananpatel.711@gmail.com>
2024-04-10 17:19:37 -0400

    Create Stripe Payment Intent

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -67,8 +67,10 @@ class LargeSecureStore {
   }
 }
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || ''
+// const supabaseUrl = 'http://127.0.0.1:54321'
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || ''
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
